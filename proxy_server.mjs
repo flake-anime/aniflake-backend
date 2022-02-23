3df3ae1bf2d7deaac0aea2c7db642b78eae621ab
Irony00 <r.rahul.developer@gmail.com>
2022-02-24 00:32:38 +0530

    integrated internal player to stream all qualities

--- proxy_server.mjs
+++ proxy_server.mjs
@@ -19,20 +19,30 @@ app.get('/player', function(req, res) {
     res.sendFile(path.join(__dirname, '/player.html'));
 });
 
-app.get('/get_streaming_links', async (req, res) => {
+app.get('/get_streaming_sources', async (req, res) => {
     const anime_id = req.query.anime_id
     const episode_number = req.query.eps_number || 1
     if(anime_id == undefined) {
         res.send("Error: anime_id is undefined")
         return
     }
+
     const dpage_url = await anicli.get_dpage_link(anime_id, episode_number)
     const links = await anicli.decrypt_link(dpage_url)
-    const data = {
-        links: links,
-        dpage_url: dpage_url
-    }
-    res.send(data)
+
+    let sources = []
+    links.forEach(link => {
+        const quality_extrator_regex = /.(\d+)p.mp4/
+        const quality = link.match(quality_extrator_regex)[1]
+        const streaming_url = '/stream?url=' + encodeURIComponent(link) + '&referer=' + encodeURIComponent(dpage_url)
+        sources.push({
+            src: streaming_url,
+            type: 'video/mp4',
+            size: quality
+        })
+    })
+
+    res.send(sources)
 })
 
 app.get("/stream", (req, res) => {
